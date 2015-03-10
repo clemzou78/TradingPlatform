7d4062f8b5972888a7d098a4318a42a98f6566f6
mickacka <mickackacka@hotmail.com>
2015-03-10 23:59:56 +0100

    dzqdzdzqdqz

--- src/com/servlets/Connexion.java
+++ src/com/servlets/Connexion.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 /* TEST GIT */
 // TEST MOI
+// DEUXIEME TEST
 public class Connexion extends HttpServlet {
 	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
 		String email = request.getParameter("username" );
