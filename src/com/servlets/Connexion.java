1fe2081c6922f2ecc2d1bd30509044f62b2c9fce
clemzou78 <clement.richet.pro@gmail.com>
2015-03-10 23:45:49 +0100

    HIHI

--- src/com/servlets/Connexion.java
+++ src/com/servlets/Connexion.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/* TEST GIT */
 public class Connexion extends HttpServlet {
 	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
 		String email = request.getParameter("username" );
