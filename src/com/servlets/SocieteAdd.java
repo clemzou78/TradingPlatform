33f66833f464444278359ea5d4f532db385cff91
clemzou78 <clement.richet.pro@gmail.com>
2015-03-12 19:44:31 +0100

    societaDD

---
+++ src/com/servlets/SocieteAdd.java
@@ -0,0 +1,47 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SocieteAdd
+ */
+@WebServlet("/SocieteAdd")
+public class SocieteAdd extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SocieteAdd() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nom = request.getParameter("nom" );
+		String mnemo = request.getParameter("mnemo");
+		String description = request.getParameter("description");
+		PrintWriter out = response.getWriter();
+		out.println(nom);
+		out.print(mnemo);
+		out.println(description);
+	}
+
+}
