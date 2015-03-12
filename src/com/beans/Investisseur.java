60baeaa44b316f43216e855c94c8c1064f7bdeb8
mickacka <mickackacka@hotmail.com>
2015-03-12 23:30:34 +0100

    Investisseur Class + Hibernate

---
+++ src/com/beans/Investisseur.java
@@ -0,0 +1,56 @@
+package com.beans;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.connection.HibernateUtil;
+
+public class Investisseur {
+	private int idInvestisseur;
+	private String nom;
+	private String prenom;
+	private User userInvestor;
+
+	
+	String getNom() {
+		return nom;
+	}
+	void setNom(String nom) {
+		this.nom = nom;
+	}
+	String getPrenom() {
+		return prenom;
+	}
+	void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	private User getUserInvestor() {
+		return userInvestor;
+	}
+	private void setUserInvestor(User userInvestor) {
+		this.userInvestor = userInvestor;
+	}
+
+	public static Investisseur createInvestissor(String nom, String prenom, String mail, String pass){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		
+		User u=new User();
+		u.setUsername(mail);
+		u.setType(UserType.Investisseur);
+		u.setPassword(User.sha256(pass));
+		session.save(u);
+
+		Investisseur s=new Investisseur();
+		s.setPrenom(prenom);
+		s.setNom(nom);
+		s.setUserInvestor(u);
+		session.save(s);
+		tx.commit();
+		
+		return s;
+	}
+	
+
+	
+}
---
+++ src/com/servlets/InvestissorAdd.java
@@ -0,0 +1,48 @@
+package com.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.beans.Investisseur;
+
+/**
+ * Servlet implementation class InvestissorAdd
+ */
+@WebServlet("/InvestissorAdd")
+public class InvestissorAdd extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InvestissorAdd() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String mail = request.getParameter("email");
+		String pass = request.getParameter("password");
+		
+		Investisseur.createInvestissor(nom, prenom, mail, pass);
+		
+	}
+
+}
