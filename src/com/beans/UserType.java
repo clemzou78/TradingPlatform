96b73ca73b9bbb991b489f4fee3296c83ccdaea0
mickacka <mickackacka@hotmail.com>
2015-03-12 21:47:43 +0100

    UserType

--- src/com/beans/UserType.java
+++ src/com/beans/UserType.java
@@ -1,7 +1,17 @@
 package com.beans;
 
 public enum UserType {
-	  	Administrateur,
-	  	Investisseur,
-	  	Societe
+	  	Administrateur("Administrateur"),
+	  	Investisseur("Investisseur"),
+	  	Societe("Societé");
+	  	
+	  	private String type;
+	  	UserType(String type){
+	  		this.type=type;
+	  	}
+	  	
+	    public String toString(){
+	        return type;
+	     }
+
 }
