13bc000827aa2fcf92605814bcb61b108fbbdee8
clemzou78 <clement.richet.pro@gmail.com>
2015-03-13 18:01:31 +0100

    oeiaoaziueoiu

--- src/com/servlets/ValidateSociete.java
+++ src/com/servlets/ValidateSociete.java
@@ -7,11 +7,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.beans.Societe;
+import com.util.RandomStringGenerator;
 
 public class ValidateSociete extends HttpServlet {
 	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
 		int idSoc = Integer.parseInt(request.getParameter("idSoc"));
-		PrintWriter out=response.getWriter();
-		out.println(idSoc);
+		String pass=RandomStringGenerator.generateRandomString(10, RandomStringGenerator.Mode.ALPHANUMERIC);
+		Societe soc=Societe.valider(idSoc, pass);
+		
+		HttpSession s=request.getSession();
+		s.setAttribute("societe", soc);
+		s.setAttribute("pass",pass);
+		request.getRequestDispatcher("confirmSocieteAdd.jsp").forward(request, response);
 	}
 }
