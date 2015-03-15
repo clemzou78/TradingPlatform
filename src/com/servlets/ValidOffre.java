ff4bcf12b9bdfc71c44946c928b32faa987c8886
mickacka <mickackacka@hotmail.com>
2015-03-15 21:48:50 +0100

    TA MERE

--- src/com/servlets/ValidOffre.java
+++ src/com/servlets/ValidOffre.java
@@ -1,11 +1,20 @@
 package com.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.beans.Investisseur;
+import com.beans.User;
+import com.ejb.ServiceContrat;
+import com.ejb.ServiceInvestisseur;
+import com.exceptions.EnchereInvalide;
+import com.exceptions.OffrePerime;
 
 /**
  * Servlet implementation class validOffre
@@ -33,7 +42,34 @@ public class ValidOffre extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		
+		
+		int idce=Integer.parseInt(request.getParameter("idContrat"));
+		String type=request.getParameter("typeContrat");
+		double montant=Double.parseDouble(request.getParameter("valeur"));
+		
+		ServiceInvestisseur si=new ServiceInvestisseur();
+		
+		HttpSession s=request.getSession();
+		Investisseur encherisseur = (new ServiceInvestisseur()).getByIdUser(  ((User) s.getAttribute("sessionUser")).getId());
+		
+		
+		ServiceContrat sc=new ServiceContrat();
+		try {
+				if(type.equals("Enchère"))
+					sc.encherir(idce, montant, encherisseur);
+				else
+					sc.fin(idce, encherisseur.getIdInvestisseur());
+			} catch (OffrePerime e) {
+	
+		
+				
+			} catch (EnchereInvalide e) {
+				
+				e.printStackTrace();
+			}
+		
 	}
 
 }
