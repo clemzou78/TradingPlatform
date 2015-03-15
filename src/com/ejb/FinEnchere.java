b4dbe48657046077e7ba23cae8141568b15b41bd
clemzou78 <clement.richet.pro@gmail.com>
2015-03-15 12:39:25 +0100

    test

--- src/com/ejb/FinEnchere.java
+++ src/com/ejb/FinEnchere.java
@@ -1,48 +1,55 @@
 package com.ejb;
 
-import java.util.Calendar;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Date;
 
-import javax.annotation.Resource;
-import javax.ejb.EJBContext;
-import javax.ejb.LocalBean;
-import javax.ejb.Schedule;
+import javax.annotation.*;
 import javax.ejb.SessionContext;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
+import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 import javax.ejb.Timeout;
 import javax.ejb.Timer;
 import javax.ejb.TimerService;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import com.beans.contrat.ContratEnchere;
 
-@Singleton
-@Startup
-@LocalBean
-public class FinEnchere {
+
+
+@Stateless
+public class FinEnchere implements EnchereRemote{
 	
 	@Resource
-	TimerService timerService;
+    TimerService timerService;
+	@Resource
+	private SessionContext context;
+
+
+	public int idce;
 	
-	ContratEnchere ce;
-	public FinEnchere(){}
-	public FinEnchere(ContratEnchere ce) {
-		this.ce=ce;
+	public FinEnchere(){
 	}
+
+
 	
-	public void createTimer(Date dateFinEnchere) {		
-		Timer timer = timerService.createTimer(dateFinEnchere, "Cretion timer enchère "+ce);
-	}
+	public void createTimer(Date dateFinEnchere, int id) {
+
+		timerService.createTimer(dateFinEnchere, id);
+		}
 
 
 	@Timeout
 	public void timeOutHandler(Timer timer){
-	   System.out.println("Fin enchère : " + ce);        
+		System.out.println(timer.getInfo()); 
+	   (new ServiceContrat()).fin(Integer.parseInt(timer.getInfo().toString()));
 	   timer.cancel();
-	   ServiceContrat sc=new ServiceContrat();
-	   sc.fin(ce.getIdContrat());
+	
 	}
+
+
+
+
+
+
+
 }
